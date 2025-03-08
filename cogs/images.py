647bcd6fad7ab3b06b20cb8622f7fa843a4791d1
“Echo” <echorw927@gmail.com>
2025-03-07 16:53:18 -0800

    Update images.py

--- cogs/images.py
+++ cogs/images.py
@@ -54,6 +54,37 @@ class Images(commands.Cog):
             err = traceback.format_exc()
             await interaction.followup.send(embed=embeds.error(err))
 
+    @app_commands.context_menu(name='ryderize')
+    async def ryderize(self, interaction, image: discord.Attachment, scale: float = 1.0):
+        await interaction.response.defer()
+        try:
+            img = await image.read()
+            with Image.open(io.BytesIO(img)) as i:
+                ryder = Image.open(os.path.abspath('./data/ryder.png'))
+
+                scale_factor = i.height / ryder.height
+                new_width = int(round(ryder.width * scale_factor * scale, 0))
+                new_height = int(round(i.height * scale, 0))
+                resized_ryder = ryder.resize((new_width, new_height))
+                resized_ryder.convert("RGBA")
+                i.convert("RGBA")
+                i.paste(resized_ryder, (i.width - new_width, 0), resized_ryder)
+                if i.height > 1080:
+                    ratio = 1080 / i.height
+                    width = ratio * i.width
+                    final = i.resize((1080, int(width)))
+                else:
+                    final = i
+                final.save('i.png')
+                i.close()
+                file = discord.File(fp='i.png', filename='i.png')
+                embed = embeds.image(img='i.png', user=interaction.user.name, command='ryderize')
+                await interaction.followup.send(file=file, embed=embed)
+        except:
+            print(traceback.print_exc())
+            err = traceback.format_exc()
+            await interaction.followup.send(embed=embeds.error(err))
+
     @commands.command(name='inspire', description='gets a quote using inspirobot')
     async def inspire(self, ctx):
         msg = await ctx.reply('waiting...', mention_author=False)
@@ -67,7 +98,7 @@ class Images(commands.Cog):
             await msg.edit(content=f"```{traceback.format_exc()}```")
 
     @app_commands.command(name='inspire')
-    async def inspirecmd(self,int):
-        pass
+    async def inspirecmd(self, ctx):
+        await ctx.response.send_message('haha imagine using the slash command\nuse `.inspire` i dont want to fix this')
 async def setup(bot):
     await bot.add_cog(Images(bot))
