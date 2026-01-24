dabf13042255a244c85aa78b6f3a64446892d347
Echo <echorw927@gmail.com>
2026-01-23 23:26:09 -0800

    d

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ bot = commands.Bot(
     command_prefix=',',
     intents=intents,
     application_id=1345951741144989766,
-    activity=discord.Game(name="lil change"),
+    activity=discord.Game(name="Playing Half-Life 3"),
 )
 
 
